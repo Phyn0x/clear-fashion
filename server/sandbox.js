a05fdf3dace71711283b17c6f611729871084ad3
Phyn0x <117830407+Phyn0x@users.noreply.github.com>
2023-02-21 14:16:35 +0100

    allProducts

--- server/sandbox.js
+++ server/sandbox.js
@@ -4,27 +4,28 @@ const montlimartbrand = require('./eshops/montlimartbrand');
 const circlesportswear = require('./eshops/circlesportswearbrand');
 const fs = require('fs')
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function scrape (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
     var products;
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
     switch(eshop){
       case 'https://www.dedicatedbrand.com/en/men/news':
         products = await dedicatedbrand.scrape(eshop);
-        fs.writeFile('dedicatedbrand.txt', JSON.stringify(products), (err) => {if (err) throw err;})
+        //fs.writeFile('dedicatedbrand.txt', JSON.stringify(products), (err) => {if (err) throw err;})
         break;
       case 'https://www.montlimart.com/99-vetements':
         products = await montlimartbrand.scrape(eshop);
-        fs.writeFile('montlimart.txt', JSON.stringify(products), (err) => {if (err) throw err;})
+        //fs.writeFile('montlimart.txt', JSON.stringify(products), (err) => {if (err) throw err;})
         break;
       case 'https://shop.circlesportswear.com/collections/collection-homme':
         products = await circlesportswear.scrape(eshop);
-        fs.writeFile('circlesportswear.txt', JSON.stringify(products), (err) => {if (err) throw err;})
+        //fs.writeFile('circlesportswear.txt', JSON.stringify(products), (err) => {if (err) throw err;})
         break;
     }
     
 
-    console.log(products);
+    //console.log(products);
+    return products;
     console.log('done');
     process.exit(0);
   } catch (e) {
@@ -33,16 +34,26 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   }
 }
 
-/*function allsandbox (){
+async function allscrape (){
   const liste = ['https://www.dedicatedbrand.com/en/men/news','https://www.montlimart.com/99-vetements','https://shop.circlesportswear.com/collections/collection-homme'] ;
   var products = [];
   for(let elem in liste){
-    products = products.concat(sandbox(liste[elem]));
+    //products = products.concat(await scrape(liste[elem]));
+    //console.log(products);
+    let temp = await scrape(liste[elem]);
+    products = products.concat(temp)
   }
   return products;
-}*/
+}
 
 const [,, eshop] = process.argv;
 
-sandbox(eshop);
-//allsandbox();
+//sandbox(eshop);
+//console.log(allsandbox());
+async function finalResult(){
+  const temp = await allscrape();
+  fs.writeFile('allProducts.json', JSON.stringify(temp), (err) => {if (err) throw err;});
+  console.log("Fini !");
+}
+
+finalResult();
