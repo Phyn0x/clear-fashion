b6610444bea4ccf97f52e0046353eaeac0472ae3
Phyn0x <117830407+Phyn0x@users.noreply.github.com>
2023-03-21 15:02:40 +0100

    api

--- server/api.js
+++ server/api.js
@@ -30,3 +30,51 @@ app.get('/', (request, response) => {
 app.listen(PORT);
 
 console.log(`📡 Running on port ${PORT}`);
+
+app.get('/products/search', async (request, response) => {
+  try{
+    const client = getClient();
+    const collection = client.db("clear-fashion").collection("products");
+
+    script = {};
+
+    var limit = request.params.limit;
+    var brand = request.params.brand;
+    var price = request.params.price;
+
+    if(limit == undefined){
+      limit = 12;
+    }
+    else{
+      limit = parseInt(limit);
+    }
+
+    if(brand){
+      script.brand = brand;
+    }
+
+    if(price){
+      script.price = price;
+    }
+
+    const result = await collection.find(script).limit(limit).toArray();
+    response.send({result : result})
+  }
+  catch(err){
+    response.send({error : "Can't search this information."})
+  }
+})
+
+app.get('/products/:id', async (request, response) => {
+  try{
+    const productId = request.params.id;
+    const script = {_id : ObjectId(productId)};
+    const client = getClient();
+    const collection = client.db("clear-fashion").collection("products");
+    const result = await collection.find(script).toArray();
+    response.send({result : result})
+  }
+  catch(err){
+    response.send({error : "Can't find item with this id"})
+  }
+})
