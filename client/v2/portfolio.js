9ff0e19d76c25ce5749aa323a9eed9ac84d15c3e
Phyn0x <117830407+Phyn0x@users.noreply.github.com>
2023-04-02 19:40:03 +0200

    aa

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -239,6 +239,7 @@ const quantile = (arr, q) => {
 
 document.addEventListener('DOMContentLoaded', async () => {
   const brand_names = await fetchBrands();
+  brand_names = brand_names.result;
   console.log(brand_names);
   spanNbBrands.innerHTML = brand_names.length;
   brand_names.unshift("All");
