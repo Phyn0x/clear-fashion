9977f4c16f58c1def8c4747cb48854db46926661
Phyn0x <117830407+Phyn0x@users.noreply.github.com>
2023-04-02 17:00:28 +0200

    màj portfolio

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -1,140 +1,274 @@
 // Invoking strict mode https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#invoking_strict_mode
 'use strict';
 
-/*
-Description of the available api
-GET https://clear-fashion-api.vercel.app/
-
-Search for specific products
-
-This endpoint accepts the following optional query string parameters:
-
-- `page` - page of products to return
-- `size` - number of products to return
-
-GET https://clear-fashion-api.vercel.app/brands
-
-Search for available brands list
-*/
-
 // current products on the page
 let currentProducts = [];
-let currentPagination = {};
+let show = 12;
+let page = 1;
+let brand = 'All';
+let price = 'All';
+let sort = 'Cheapest';
+let favorite_products = [];
+const current_date = Date.now();
 
 // instantiate the selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
-const sectionProducts = document.querySelector('#products');
+const selectBrand = document.querySelector('#brand-select');
+const selectPrice = document.querySelector('#price-select');
+const selectSort = document.querySelector('#sort-select');
 const spanNbProducts = document.querySelector('#nbProducts');
+const spanNbBrands = document.querySelector('#nbBrands');
+const spanPercentile50 = document.querySelector('#percentile50');
+const spanPercentile90 = document.querySelector('#percentile90');
+const spanPercentile95 = document.querySelector('#percentile95');
+const spanNbSearchProducts = document.querySelector('#nbSearchProducts');
+const sectionSearchProducts = document.querySelector('#searchProducts');
+const spanNbFavoriteProducts = document.querySelector('#nbFavoriteProducts');
+const sectionFavoriteProducts = document.querySelector('#favoriteProducts');
 
 /**
- * Set global value
- * @param {Array} result - products to display
- * @param {Object} meta - pagination meta info
+ * Fetch API
  */
-const setCurrentProducts = ({result, meta}) => {
-  currentProducts = result;
-  currentPagination = meta;
-};
 
-/**
- * Fetch products from api
- * @param  {Number}  [page=1] - current page to fetch
- * @param  {Number}  [size=12] - size of the page
- * @return {Object}
- */
-const fetchProducts = async (page = 1, size = 12) => {
+const fetchProducts = async (show=12, page=1, brand="",price="") => {
   try {
-    const response = await fetch(
-      `https://clear-fashion-flax-six.vercel.app?page=${page}&size=${size}`
-    );
+    let url = `https://clear-fashion-api-jaoudet.vercel.app/products/search?page=${page}&limit=${show}&brand=${brand}&price=${price}`;
+    ///let url = `http://localhost:8092/products/search?page=${page}&limit=${show}&brand=${brand}&price=${price}`;
+    console.log(url);
+    const response = await fetch(url);
     const body = await response.json();
 
-    if (body.success !== true) {
-      console.error(body);
-      return {currentProducts, currentPagination};
-    }
-
+    const currentPage = body.currentPage;
+    const totalPages = body.totalPages;
+    spanNbSearchProducts.innerHTML = body.totalCount + ' products found';
+    const options = Array.from(
+      {'length': totalPages},
+      (value, index) => `<option value="${index + 1}">${index + 1}</option>`
+    ).join('');
+    selectPage.innerHTML = options;
+    selectPage.selectedIndex = currentPage - 1;
     return body.data;
   } catch (error) {
     console.error(error);
-    return {currentProducts, currentPagination};
+    return currentProducts;
+  }
+};
+
+const fetchAllProducts = async () => {
+  try {
+    const response = await fetch(
+      'https://clear-fashion-api-jaoudet.vercel.app/products'
+      ///`http://localhost:8092/products`
+    );
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error(error);
+    return currentProducts;
+  }
+};
+
+const fetchBrands = async () => {
+  try {
+    const response = await fetch(
+      'https://clear-fashion-api-jaoudet.vercel.app/brands'
+      //`http://localhost:8092/brands`
+    );
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error(error);
+    return currentProducts;
+  }
+};
+
+const fetchSortProducts = async (sort=-1) => {
+  try {
+    const response = await fetch(
+      `https://clear-fashion-api-jaoudet.vercel.app/sort?sort=${sort}`
+      ///`http://localhost:8092/sort?sort=${sort}`
+    );
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error(error);
+    return currentProducts;
   }
 };
 
 /**
- * Render list of products
- * @param  {Array} products
+ * Favorite products
  */
-const renderProducts = products => {
-  const fragment = document.createDocumentFragment();
-  const div = document.createElement('div');
+
+async function changeFavorite(id) {
+  if (favorite_products.find(element => element._id === id)) {
+    favorite_products = favorite_products.filter(item => item._id !== id);
+  }
+  else {
+    favorite_products.push(currentProducts.find(element => element._id === id));
+  }
+  document.getElementById(id).getElementsByTagName('button')[0].innerText = textFavorite(id);
+  renderFavoriteProducts();
+}
+
+function textFavorite(id) {
+  let text = "";
+  if (favorite_products.find(element => element._id === id)) {
+    text = "Remove favorite";
+  }
+  else {
+    text = "Add favorite";
+  }
+  return text;
+}
+
+/**
+ * Render list of products
+ */
+
+const renderSearchProducts = products => {
+  currentProducts = products;
   const template = products
     .map(product => {
       return `
-      <div class="product" id=${product.uuid}>
+      <div class="product" id=${product._id}>
         <span>${product.brand}</span>
-        <a href="${product.link}">${product.name}</a>
-        <span>${product.price}</span>
+        <a href="${product.link}" target="_blank">${product.name}</a>
+        <span>${product.price}€</span>
+        <span>${product.caracteristique}</span>
+        <button onclick="changeFavorite('${product._id}')">${textFavorite(product._id)}</button>
       </div>
     `;
     })
     .join('');
 
-  div.innerHTML = template;
-  fragment.appendChild(div);
-  sectionProducts.innerHTML = '<h2>Products</h2>';
-  sectionProducts.appendChild(fragment);
+  sectionSearchProducts.innerHTML = template;
 };
 
-/**
- * Render page selector
- * @param  {Object} pagination
- */
-const renderPagination = pagination => {
-  const {currentPage, pageCount} = pagination;
-  const options = Array.from(
-    {'length': pageCount},
-    (value, index) => `<option value="${index + 1}">${index + 1}</option>`
-  ).join('');
-
-  selectPage.innerHTML = options;
-  selectPage.selectedIndex = currentPage - 1;
-};
-
-/**
- * Render page selector
- * @param  {Object} pagination
- */
-const renderIndicators = pagination => {
-  const {count} = pagination;
-
-  spanNbProducts.innerHTML = count;
-};
-
-const render = (products, pagination) => {
-  renderProducts(products);
-  renderPagination(pagination);
-  renderIndicators(pagination);
+const renderFavoriteProducts = products => {
+  const template = favorite_products
+    .map(product => {
+      return `
+      <div class="product" id=${product._id}>
+        <span>${product.brand}</span>
+        <a href="${product.link}" target="_blank">${product.name}</a>
+        <span>${product.price}€</span>
+        <span>${product.caracteristique}</span>
+        <button onclick="changeFavorite('${product._id}')">${textFavorite(product._id)}</button>
+      </div>
+    `;
+    })
+    .join('');
+  
+  spanNbFavoriteProducts.innerHTML = favorite_products.length + (favorite_products.length > 1 ? ' favorite products' : ' favorite product');
+  sectionFavoriteProducts.innerHTML = template;
 };
 
 /**
  * Declaration of all Listeners
  */
 
-/**
- * Select the number of products to display
- */
 selectShow.addEventListener('change', async (event) => {
-  const products = await fetchProducts(currentPagination.currentPage, parseInt(event.target.value));
-
-  setCurrentProducts(products);
-  render(currentProducts, currentPagination);
+  show = event.target.value;
+  page = 1;
+  let products = await fetchProducts(show=show, page=1, brand="", price="")
+  renderSearchProducts(products);
 });
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const products = await fetchProducts();
+selectPage.addEventListener('change', async (event) => {
+  page = event.target.value;
+  let products = await fetchProducts(show=12, page=page, brand="", price="")
+  renderSearchProducts(products);
+});
 
-  setCurrentProducts(products);
-  render(currentProducts, currentPagination);
+selectBrand.addEventListener('change', async (event) => {
+  brand = event.target.value;
+  if(brand=='All'){
+    brand="";
+  }
+  page = 1;
+  let products = await fetchProducts(show=show, page=page, brand=brand, price="")
+  renderSearchProducts(products);
+});
+
+selectPrice.addEventListener('change', async (event) => {
+  price = event.target.value;
+  if(price=='All'){
+    price="";
+  }
+  page = 1;
+  let products = await fetchProducts(show=show, page=page, brand="", price=price)
+  renderSearchProducts(products);
+});
+
+selectSort.addEventListener('change', async (event) => {
+  sort = event.target.value;
+  page = 1;
+  let products = await fetchSortProducts();
+
+  let listeOfPdts=[];
+
+  if(sort!='Default'){
+
+    if(sort=='Cheapest'){
+      listeOfPdts = await fetchSortProducts(1);
+    }
+    else if(sort=='Most expensive'){
+      listeOfPdts = await fetchSortProducts(-1);
+    }
+
+  }
+  else{
+    listeOfPdts = products
+  }
+  
+  products=listeOfPdts;
+
+  renderSearchProducts(products);
+});
+
+/**
+ * Launched on page load
+ */
+
+const quantile = (arr, q) => {
+  const sorted = arr.sort((a, b) => a - b);
+  const pos = (sorted.length - 1) * q;
+  const base = Math.floor(pos);
+  const rest = pos - base;
+  if (sorted[base + 1] !== undefined) {
+      return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+  } else {
+      return sorted[base];
+  }
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const brand_names = await fetchBrands();
+  spanNbBrands.innerHTML = brand_names.length;
+  
+  brand_names.unshift("All");
+  const brands = Array.from(
+    brand_names,
+    value => `<option value="${value}">${value}</option>`
+  ).join('');
+  
+  selectBrand.innerHTML = brands;
+  
+  let products = await fetchProducts();
+  renderSearchProducts(products);
+
+  const all_products = await fetchAllProducts();
+  spanNbProducts.innerHTML = all_products.length;
+  
+  
+  let prices = [];
+
+  for (let product_id in all_products) {
+    prices.push(all_products[product_id].price);
+  }
+  spanPercentile50.innerHTML = Math.round(quantile(prices, 0.50));
+  spanPercentile90.innerHTML = Math.round(quantile(prices, 0.90));
+  spanPercentile95.innerHTML = Math.round(quantile(prices, 0.95));
 });
